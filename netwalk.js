ad136f4d256fce54735517783ce8dbea029ccdd2
Brandon DeRosier <bdero@mit.edu>
2015-04-08 00:04:43 -0400

    Add board size options

--- netwalk.js
+++ netwalk.js
@@ -3,11 +3,24 @@
   /**
    * Represents a game of Netwalk.
    * @constructor
-   * @param {object} parent jQuery selector of the element in which the Netwalk
-   *                        board will be rendered.
+   * @param {object} parent - jQuery selector of the element in which the
+   *                          Netwalk board will be rendered.
+   * @param {object} options - Optional. An object containing key-value pairs
+   *                           for additional configuration options.
    */
-  function Netwalk(parent) {
-    this.renderer = new NetwalkRenderer(parent);
+  function Netwalk(parent, options) {
+    this.options = options || {};
+
+    // Configure option defaults
+    this.options.columns = this.options.columns || 10;
+    this.options.rows = this.options.rows || 10;
+
+    // Initialize the renderer
+    this.renderer = new NetwalkRenderer(
+      parent,
+      this.options.columns,
+      this.options.rows
+    );
   }
 
 
@@ -15,11 +28,14 @@
    * Stores the parent element and synchronizes the Netwalk board based on
    * state changes.
    * @constructor
-   * @param {object} parent jQuery selector of the element in which the Netwalk
-   *                        board will be rendered.
+   * @param {object} parent - jQuery selector of the element in which the
+   *                          Netwalk board will be rendered.
+   * @param {integer} columns - The number of columns in the Netwalk board.
+   * @param {integer} rows - The number of rows in the Netwalk board.
    */
-  function NetwalkRenderer(parent) {
+  function NetwalkRenderer(parent, columns, rows) {
     this.parent = parent;
+    this.size = { x: columns, y: rows };
   }
 
 
