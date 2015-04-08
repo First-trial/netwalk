db3e20a54bebac47110fbcc8e6b22e28e7eafed9
Brandon DeRosier <bdero@mit.edu>
2015-04-07 23:45:02 -0400

    Scaffold classes and initialize

--- netwalk.js
+++ netwalk.js
@@ -1,3 +1,29 @@
-$(document).ready(function() {
-  // netwalk.js
-});
+(function() {
+
+  /**
+   * Represents a game of Netwalk.
+   * @constructor
+   * @param {object} parent jQuery selector of the element in which the Netwalk
+   *                        board will be rendered.
+   */
+  function Netwalk(parent) {
+    this.renderer = new NetwalkRenderer(parent);
+  }
+
+
+  /**
+   * Stores the parent element and synchronizes the Netwalk board based on
+   * state changes.
+   * @constructor
+   * @param {object} parent jQuery selector of the element in which the Netwalk
+   *                        board will be rendered.
+   */
+  function NetwalkRenderer(parent) {
+    this.parent = parent;
+  }
+
+
+  // Place the Netwalk object in the global scope
+  window.Netwalk = Netwalk;
+
+})();
